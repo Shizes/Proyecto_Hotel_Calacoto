6d470fa5ff495bb48287d56258a5854e877bfc50
Reyshel <reyshelortiz03@gmail.com>
2024-11-13 21:37:12 -0400

    cambio2navbar

---
+++ src/app/contactos/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactosPage = () => {
+  return (
+    <div>ContactosPage</div>
+  )
+}
+
+export default ContactosPage
---
+++ src/app/habitaciones/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HabitacionesPage = () => {
+  return (
+    <div>HabitacionesPage</div>
+  )
+}
+
+export default HabitacionesPage
